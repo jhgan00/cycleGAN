c9657cf7d5548a68c9e3754b165ba6c6ce1155d2
Junghyun Gan <jhgan00@gmail.com>
2020-07-16 23:45:37 +0900

    Update loss obj

--- modules/loss.py
+++ modules/loss.py
@@ -1,23 +1,25 @@
 import tensorflow as tf
-from tensorflow.keras.losses import mean_squared_error, mean_absolute_error
+from tensorflow.keras.losses import MeanSquaredError, MeanAbsoluteError
 
+mse = MeanSquaredError()
+mae = MeanAbsoluteError()
 
 def discriminator_loss(real, generated):
-    real_loss = mean_squared_error(tf.ones_like(real), real)
-    generated_loss = mean_squared_error(tf.zeros_like(generated), generated)
+    real_loss = mse(tf.ones_like(real), real)
+    generated_loss = mse(tf.zeros_like(generated), generated)
     total_disc_loss = real_loss + generated_loss
     return total_disc_loss * 0.5
 
 
 def generator_loss(generated):
-    return mean_squared_error(tf.ones_like(generated), generated)
+    return mse(tf.ones_like(generated), generated)
 
 
 def cycle_loss(real_image, cycled_image, LAMBDA=10):
-    loss = tf.math.reduce_mean(tf.math.abs(real_image - cycled_image))
+    loss = mae(real_image, cycled_image)
     return loss * LAMBDA
 
 
 def identity_loss(real_image, same_image, LAMBDA=10):
-    loss = tf.math.reduce_mean(tf.math.abs(real_image - same_image))
+    loss = mae(real_image, same_image)
     return loss * LAMBDA * 0.5
